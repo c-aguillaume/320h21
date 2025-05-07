22963934dc5b99f6788baa3f1c19100b1191fc3b
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-05-07 13:04:14 -0400

    update to styling

--- react-remake/src/Components/Body.jsx
+++ react-remake/src/Components/Body.jsx
@@ -5,6 +5,10 @@ export default function Body() {
     <div>
         <Article />
         <Article
+        time="11/14/20"
+        title="On the Street in Jersey City"
+        image=""
+
         />
         </div>
   )
@@ -15,8 +19,8 @@ export default function Body() {
 export function Article(props) {
   return (
     <div>
-        <time>11/12/20</time>
-        <h3>On the Street in Brooklyn</h3>
+        <time>{props.time}</time>
+        <h3>{props.title}</h3>
         <img src='/src/assets/images/blog-image-1.jpg'></img>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni in quis ex dolores? Eos illo cumque maiores dolores. Ad, odit sunt culpa, consequuntur eos reiciendis ex ut earum ab deleniti, rem harum nobis enim quisquam officia. Voluptatibus autem officiis ullam. Consequatur explicabo recusandae exercitationem. Cumque facilis provident recusandae iure, optio iste libero harum sapiente dolorum assumenda excepturi ex culpa quod iusto placeat quae molestiae nesciunt est sequi nihil laudantium. Perspiciatis quasi tempore nam esse dolore recusandae? Ab ullam quis autem? Mollitia doloribus assumenda nobis soluta delectus hic impedit tempora at quis dolor quia quae rem a ullam illum laborum, labore nam. Rem eum neque mollitia molestiae ex quisquam, aliquam accusantium in atque doloribus minus nobis, aut possimus doloremque excepturi fugiat, cum odit ducimus? Voluptatem iste ad modi doloremque hic eum, neque recusandae unde necessitatibus! Quidem ea vel dignissimos laudantium! Nemo illo, facilis totam nesciunt sunt nisi! Quo officiis nesciunt deserunt recusandae vitae ducimus asperiores, nisi illo sed alias nobis maiores, accusamus voluptatibus totam ratione omnis laboriosam blanditiis deleniti quidem accusantium. Tempore quam consectetur deserunt iusto est id quis nemo non praesentium nobis dolor explicabo veniam debitis quod veritatis quia totam odit ea suscipit ab fugiat officia, reiciendis quasi. Consequatur.</p>
     </div>
--- react-remake/src/Components/Header.jsx
+++ react-remake/src/Components/Header.jsx
@@ -1,20 +1,14 @@
-import React from 'react'
+import React from "react";
+import Nav from "./Nav";
 
 export default function Header() {
   return (
     <div>
-        <h1>Sartre's List</h1>
-        <h2>Better-Dressed People</h2>
-        <nav>
-            <ul>
-                <li><a href="#">Women's</a></li>
-                <li><a href="#">Men's</a></li>
-                <li><a href="#">On the Street</a></li>
-                <li><a href="#">The Catwalk</a></li>
-                <li><a href="#">AdWatch</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-        </nav>
+      <header className="site-header">
+        <h1 className="main-title">Sartre's List</h1>
+        <h2 className="subtitle">Better-Dressed People</h2>
+        <Nav />
+      </header>
     </div>
-  )
+  );
 }
