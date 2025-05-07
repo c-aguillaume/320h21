88d45df457b6a63ecacc779a638c00bf743cc527
c-aguillaume <175580634+c-aguillaume@users.noreply.github.com>
2025-05-07 02:06:09 -0400

    set up and update components

---
+++ react-remake/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+import Header from './Components/Header'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
+
+export default function App() {
+  return (
+    <div>
+    <Header />
+    <Body />
+    <Footer />
+    </div>
+  )
+}
+
+
---
+++ react-remake/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Sartre's List</h1>
+        <h2>Better-Dressed People</h2>
+        <nav>
+            <ul>
+                <li><a href="#">Women's</a></li>
+                <li><a href="#">Men's</a></li>
+                <li><a href="#">On the Street</a></li>
+                <li><a href="#">The Catwalk</a></li>
+                <li><a href="#">AdWatch</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
